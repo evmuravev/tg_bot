165734c5969afb59a9197734071d021f462822fb
Evgenii Muravev <muravev.mail@gmail.com>
2023-10-07 16:56:57 +0300

    update text

--- handlers/date_response/date_response.py
+++ handlers/date_response/date_response.py
@@ -101,5 +101,5 @@ async def date_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.answer_callback_query(
                 callback_query_id=update.callback_query.id,
                 show_alert=True,
-                text='Ваш отклик с вашим контактом доставлен!\nТеперь пользователь может вам написать🤞',
+                text='Ваш отклик с вашим контактом доставлен пользователю!💌\nТеперь он может вам написать🤞',
             )
