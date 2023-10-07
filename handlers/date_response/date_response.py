8809855d45226b098f908f3394913f6f18679a0a
Evgenii Muravev <muravev.mail@gmail.com>
2023-10-07 16:43:07 +0300

    fix typo

--- handlers/date_response/date_response.py
+++ handlers/date_response/date_response.py
@@ -101,5 +101,5 @@ async def date_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await context.bot.answer_callback_query(
                 callback_query_id=update.callback_query.id,
                 show_alert=True,
-                text='Ваш отклик с вашим контактом доставлен! Теперь отсалось дождаться сообщения от человека🤞',
+                text='Ваш отклик с вашим контактом доставлен!\n Теперь пользователь может вам написать🤞',
             )
